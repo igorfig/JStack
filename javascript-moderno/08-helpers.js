6c51edea9d110c0e07a013a7ef84724cc487c42e
Igor Figueiredo <igorfigueiredors@gmail.com>
2022-09-17 09:25:17 -0300

    .includes, .startsWith e .endsWith

---
+++ javascript-moderno/08-helpers.js
@@ -0,0 +1,11 @@
+const tech = 'Node.js';
+
+const array = ['Node.js', 'React', 'TypeScript'];
+
+const includesString = tech.includes('Node');
+const includesArray = array.includes('Node.js')
+
+
+const startsWith = tech.endsWith('js');
+
+console.log({ startsWith });
