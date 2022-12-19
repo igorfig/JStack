750a3dcfe16599047a5d18c250c9a705cfef9710
Igor Figueiredo <igorfigueiredors@gmail.com>
2022-12-19 14:52:02 -0300

    Trabalhando com States

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,47 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 import Post from "./Post";
 
-const posts = [
-  {
-    title: 'Title#01',
-    subtitle: 'Sub#01',
-    likes: 20
-  },
-
-  {
-    title: 'Title#02',
-    subtitle: 'Sub#02',
-    likes: 10
-  },
-
-  {
-    title: 'Title#03',
-    subtitle: 'Sub#03',
-    likes: 50
-  },
-  {
-    title: 'Title#04',
-    subtitle: 'Sub#04',
-    likes: 50
-  },
-]
-
 export function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: "Title#01",
+      subtitle: "Sub#01",
+      likes: 20,
+    },
+
+    {
+      id: Math.random(),
+      title: "Title#02",
+      subtitle: "Sub#02",
+      likes: 10,
+    },
+
+    {
+      id: Math.random(),
+      title: "Title#03",
+      subtitle: "Sub#03",
+      likes: 50,
+    },
+    {
+      id: Math.random(),
+      title: "Title#04",
+      subtitle: "Sub#04",
+      likes: 50,
+    },
+  ]);
+
+  function handleRefresh() {
+    setPosts((prevState) => ([
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title#${prevState.length + 1}`,
+        subtitle: `Sub#${prevState.length + 1}`,
+        likes: 50,
+      },
+    ]));
+  }
+
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
       <hr />
-      {posts.map(post => (
-        <Post key={post.title} post={{
-          title: post.title,
-          subtitle: post.subtitle
-        }} likes={post.likes}/>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+          likes={post.likes}
+        />
       ))}
-     {/*  <Post
+      {/*  <Post
         likes={20}
         post={{
           title: "Título da notícia 01",
