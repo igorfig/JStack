fa11b5dc634abee9193d389c1be7b8beb48fa753
Igor Figueiredo <igorfigueiredors@gmail.com>
2022-12-19 12:06:28 -0300

    Definindo tipos para as props

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -6,15 +6,31 @@ import Post from "./Post";
 export function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Posts da semana</h2>
       </Header>
-      
       <hr />
-
-      <Post title="Título da notícia 01" subtitle="Subtítulo da notícia 01" />
-      <Post title="Título da notícia 02" subtitle="Subtítulo da notícia 02" />
-      <Post title="Título da notícia 03" subtitle="Subtítulo da notícia 03" />
+      <Post
+        likes={20}
+        post={{
+          title: "Título da notícia 01",
+          subtitle: "Subtítulo da notícia 01",
+        }}
+      />
+      <Post
+        likes={50}
+        post={{
+          title: "Título da notícia 02",
+          subtitle: "Subtítulo da notícia 02",
+        }}
+      />
+      <Post
+        likes={10}
+        post={{
+          title: "Título da notícia 03",
+          subtitle: "Subtítulo da notícia 03",
+        }}
+      />
     </>
   );
 }
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 export default function Post(props) {
   return (
     <article>
-      <strong>{props.title}</strong>
+      <strong>{props.post.title}</strong>
       <br />
-      <small>{props.subtitle}</small>
+      <small>{props.post.subtitle}</small>
+      <br />
+      <strong>Média: {props.likes / 2}</strong>
     </article>
   );
+}
+
+Post.propTypes = {
+  likes: PropTypes.number.isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+  }).isRequired
 }
