592e411e42ff8229f55ef99f13017eb8b3c27284
Igor Figueiredo <igorfigueiredors@gmail.com>
2022-12-19 14:26:18 -0300

    Renderizando listas

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -3,6 +3,31 @@ import Header from "./Header";
 
 import Post from "./Post";
 
+const posts = [
+  {
+    title: 'Title#01',
+    subtitle: 'Sub#01',
+    likes: 20
+  },
+
+  {
+    title: 'Title#02',
+    subtitle: 'Sub#02',
+    likes: 10
+  },
+
+  {
+    title: 'Title#03',
+    subtitle: 'Sub#03',
+    likes: 50
+  },
+  {
+    title: 'Title#04',
+    subtitle: 'Sub#04',
+    likes: 50
+  },
+]
+
 export function App() {
   return (
     <>
@@ -10,7 +35,13 @@ export function App() {
         <h2>Posts da semana</h2>
       </Header>
       <hr />
-      <Post
+      {posts.map(post => (
+        <Post key={post.title} post={{
+          title: post.title,
+          subtitle: post.subtitle
+        }} likes={post.likes}/>
+      ))}
+     {/*  <Post
         likes={20}
         post={{
           title: "Título da notícia 01",
@@ -30,7 +61,7 @@ export function App() {
           title: "Título da notícia 03",
           subtitle: "Subtítulo da notícia 03",
         }}
-      />
+      /> */}
     </>
   );
 }
