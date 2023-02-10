67498708eef891162cd361583525ddc7ba2c2d02
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-02-10 10:03:42 -0300

    Implementando Theming com Styled Components

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,88 +1,18 @@
-import React, { useState } from "react";
-import Header from "./Header";
-import Post from "./Post";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
-import styles from './App.scss'
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
 
-import { ThemeProvider } from './hooks/useTheme'
+import themes from './styles/themes/'
 
-export function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: Math.random(),
-      title: "Title#01",
-      subtitle: "Sub#01",
-      likes: 20,
-      read: true,
-      removed: false,
-    },
-
-    {
-      id: Math.random(),
-      title: "Title#02",
-      subtitle: "Sub#02",
-      likes: 10,
-      read: true,
-      removed: false,
-    },
-
-    {
-      id: Math.random(),
-      title: "Title#03",
-      subtitle: "Sub#03",
-      likes: 50,
-      read: false,
-      removed: false,
-    },
-    {
-      id: Math.random(),
-      title: "Title#04",
-      subtitle: "Sub#04",
-      likes: 50,
-      read: false,
-      removed: false,
-    },
-  ]);
-
-  function handleRefresh() {
-    setPosts((prevState) => ([
-      ...prevState,
-      {
-        id: Math.random(),
-        title: `Title#${prevState.length + 1}`,
-        subtitle: `Sub#${prevState.length + 1}`,
-        likes: 50,
-      },
-    ]));
-  }
-
-  function handleRemovePost(postId) {
-    setPosts(prevState => prevState.map(
-      post => (
-        post.id === postId
-          ? { ...post, removed: true }
-          : post 
-      )
-    ))
-  }
-
+function App() {
   return (
-    <ThemeProvider>
-      <Header>
-        <h2 className={styles.title}>
-          Posts da semana
-          <button onClick={handleRefresh}>Atualizar</button>
-        </h2>
-      </Header>
-      <hr />
-      {posts.map((post) => (
-        <Post
-          onRemove={handleRemovePost}
-          key={post.id}
-          post={post}
-          likes={post.likes}
-        />
-      ))}
+    <ThemeProvider theme={themes.light}>
+      <GlobalStyle />
+      <Layout />
     </ThemeProvider>
   );
-}
+};
+
+export default App;
