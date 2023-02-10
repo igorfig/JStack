f8cfb8bb546c0b3841dbe8f51b5f7b361bf70c93
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-02-10 10:30:53 -0300

    Implementando a troca de temas

--- react-fundamentos/src/components/Footer/index.js
+++ react-fundamentos/src/components/Footer/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({ selectedTheme, onToggleTheme }) {
   return (
     <Container>
       <span>JStack's Blog. Todos os direitos reservados.</span>
-      <button type="button">🌞</button>
+      <button 
+        type="button" 
+        onClick={onToggleTheme}>
+          {selectedTheme === 'dark' ? "🌞" : '🌚'}
+        </button>
     </Container>
   );
 }
--- react-fundamentos/src/components/Header/index.js
+++ react-fundamentos/src/components/Header/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header() {
+export default function Header({ selectedTheme, onToggleTheme }) {
   return (
     <Container>
       <h1>JStack's Blog</h1>
-      <button type="button">🌞</button>
+      <button 
+        type="button" 
+        onClick={onToggleTheme}>
+          {selectedTheme === 'dark' ? '🌞' : '🌚'}
+        </button>
     </Container>
   );
 }
