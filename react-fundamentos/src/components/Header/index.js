b0661ce6ffe7b2feb16aee0e2eec86570f1da14e
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-04-13 21:29:30 -0300

    Tipando as props

--- react-fundamentos/src/components/Header/index.js
+++ react-fundamentos/src/components/Header/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useTheme } from '../../hooks/useTheme';
 import { Container } from './styles';
 
 export default class Header extends React.Component {
+  static propTypes = {
+    selectedTheme:  PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+  }
   render() {
     const { selectedTheme, onToggleTheme } = this.props;
     return (
