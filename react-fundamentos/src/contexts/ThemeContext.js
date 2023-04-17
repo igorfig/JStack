5bc3902de988460d2d159f52e98375d1dd57911e
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-04-17 18:51:12 -0300

    Class Components | Salvando o theme no localStorage

--- react-fundamentos/src/contexts/ThemeContext.js
+++ react-fundamentos/src/contexts/ThemeContext.js
@@ -4,14 +4,26 @@ export const ThemeContext = createContext();
 
 
 export class ThemeProvider extends React.Component {
-	state = {
-		theme: 'dark',
+	constructor(props) {
+		super(props);
+		let theme = 'dark';
+		try {
+			theme = JSON.parse(localStorage.getItem('theme'));
+		} catch(err) {
+			console.log(err);
+		}	
+
+		this.state = {
+			theme,
+		}
 	}
 
 	handleToggleTheme = () => {
 		this.setState((prevState) => ({
 			theme: prevState.theme === 'dark' ? 'light' : 'dark'
-		}))
+		}), () => {
+			localStorage.setItem('theme', JSON.stringify(this.state.theme));
+		})
 	}
 
 	render() {
