9a3a9f514947f15201609c044da8c1c83273e803
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-04-19 19:31:57 -0300

    React Router v5 | Parâmetros e query params em Class Components

--- react-fundamentos/src/pages/Post/index.js
+++ react-fundamentos/src/pages/Post/index.js
@@ -2,7 +2,23 @@ import React, { useMemo } from 'react';
 
 import { useParams, useLocation } from 'react-router-dom';
 
-export default function Post() {
+export default class Post extends React.Component {
+	constructor(props) {
+		super(props);
+
+
+		const { search } = this.props.location;
+
+		this.queryParams = new URLSearchParams(search);
+	}
+
+	render() {
+		console.log(this.queryParams.get('query'))
+		return <h1>Post Page</h1>;
+	}
+}
+
+/*export default function Post() {
 	const params = useParams();
 	const { search } = useLocation();
 
@@ -11,4 +27,4 @@ export default function Post() {
 	console.log(queryParams.get(''));
 
 	return <h1>Post Page</h1>
-}
+}*/
