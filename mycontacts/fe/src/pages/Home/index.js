bed2ebee58dc515e3966e45e53dd249f3a8fc59a
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-05-01 19:10:34 -0300

    MyContacts Front-end | Criando a página Home e refatorando o Header

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -1,3 +1,91 @@
+import { Container, InputSearchContainer, Header, ListContainer, Card } from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-	return 'Homepage';
+	return (
+		<Container>
+			<InputSearchContainer>
+				<input type="text" placeholder="Pesquise pelo nome..."/>
+			</InputSearchContainer>
+
+			<Header>
+				<strong>3 contatos</strong>
+				<a href="/">Novo contato</a>
+			</Header>
+
+			<ListContainer>
+				<header>
+					<button type="button" className="sort-button">
+						<span>Nome</span>
+						<img src={arrow} alt="Arrow" />
+					</button>
+				</header>
+
+
+				<Card>
+					<div className="info">
+						<div className="contact-name">
+							<strong>Igor Figueiredo</strong>
+							<small>instagram</small>
+						</div>
+						<span>igorfs@gmail.com</span>
+						<span>(31) 99999-9999</span>
+					</div>
+
+					<div className="actions">
+						<a href="/">
+							<img src={edit} alt="Edit" />
+						</a>
+
+						<button>
+							<img src={trash} alt="Trash" />
+						</button>	
+					</div>	
+				</Card>
+				<Card>
+					<div className="info">
+						<div className="contact-name">
+							<strong>Igor Figueiredo</strong>
+							<small>instagram</small>
+						</div>
+						<span>igorfs@gmail.com</span>
+						<span>(31) 99999-9999</span>
+					</div>
+
+					<div className="actions">
+						<a href="/">
+							<img src={edit} alt="Edit" />
+						</a>
+
+						<button>
+							<img src={trash} alt="Trash" />
+						</button>	
+					</div>	
+				</Card>
+				<Card>
+					<div className="info">
+						<div className="contact-name">
+							<strong>Igor Figueiredo</strong>
+							<small>instagram</small>
+						</div>
+						<span>igorfs@gmail.com</span>
+						<span>(31) 99999-9999</span>
+					</div>
+
+					<div className="actions">
+						<a href="/">
+							<img src={edit} alt="Edit" />
+						</a>
+
+						<button>
+							<img src={trash} alt="Trash" />
+						</button>	
+					</div>	
+				</Card>
+			</ListContainer>
+
+		</Container>	
+	);
 }
