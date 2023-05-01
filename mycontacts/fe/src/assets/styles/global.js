d7890166b7b8ac35fde603c23d4c8e884eeb595c
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-04-30 21:43:04 -0300

    MyContacts Front-end | Criando a listagem de contatos

--- mycontacts/fe/src/assets/styles/global.js
+++ mycontacts/fe/src/assets/styles/global.js
@@ -14,6 +14,7 @@ export default createGlobalStyle`
 	body {
 		background: ${({ theme }) => theme.colors.background};
 		font-size: 16px;
+		color: ${({ theme }) => theme.colors.gray[900]};
 	}
 
 	button {
--- mycontacts/fe/src/assets/styles/themes/default.js
+++ mycontacts/fe/src/assets/styles/themes/default.js
@@ -7,5 +7,9 @@ export default {
 			main: '#5061FC',
 			darker: '#3346F0',
 		},
-	}
+		gray: {
+			900: '#222222',
+			200: '#BCBCBC'
+		}
+	},
 };
