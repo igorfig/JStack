9b93e70cf730452e42448bb6c01457ac4a04465b
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-08-14 19:25:09 -0300

    Abstraindo a lógica de erros em um Custom Hook

--- mycontacts/fe/src/components/ContactForm/index.js
+++ mycontacts/fe/src/components/ContactForm/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import isEmailValid from '../../utils/isEmailValid';
-
+import useErrors from '../../hooks/useErrors';
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -16,21 +16,19 @@ export default function ContactForm({ buttonLabel }) {
 	const [phone, setPhone] = useState('');
 	const [category, setCategory] = useState('');
 	const [errors, setErrors] = useState([]);
-	console.log(errors);
+	const { setError, removeError, getErrorMessageByFieldName} = useErrors();
+
 	const handleNameChange = (event) => { 
 		setName(event.target.value); 
 
 		if(!event.target.value) {
-			setErrors(prevState => [
-				...prevState,
-				{
-					field: 'name', message:' Nome é obrigatório.',
-				}
-			]);
+			setError({ field: 'name', message: 'Nome é obrigatório.' });
 		} else {
 			setErrors(prevState => prevState.filter(
 				(error) => error.field !== 'name'
 			));
+
+			removeError('name');
 		}
 	}
 
@@ -38,28 +36,10 @@ export default function ContactForm({ buttonLabel }) {
 		setEmail(event.target.value);
 
 		if(event.target.value && !isEmailValid(event.target.value)) {
-			const errorAlreadyExists = errors.find(error => error.field === 'email');
-
-			if(errorAlreadyExists) {
-				return;
-			}
-
-			setErrors(prevState => [
-				...prevState,
-				{ 
-					field: 'email', message: 'O e-mail inválido.',
-				}
-			]);
+			setError({ field: 'email', message: 'E-mail inválido.' })
 		} else {
-			setErrors(prevState => prevState.filter(
-				error => error.field !== 'email'
-			));
+			removeError('email');
 		}
-	}
-
-
-	const getErrorMessageByFieldName = (fieldName) => {
-		return errors.find(error => error.field === fieldName)?.message;
 	}
 
 	const handleSubmit = (event) => {
