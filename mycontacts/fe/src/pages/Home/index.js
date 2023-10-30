b82f43e97d1d4a35e111364838c83c15752ec423
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-10-30 19:21:15 -0300

    Fazendo o parse do body da response

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -53,14 +53,13 @@ export default function Home() {
 	);
 }
 
-fetch('http://localhost:3001/contacts', {
-	method: 'DELETE',
-	headers: new Headers({
-		'X-App-ID': '123',
-	})
-})
-	.then((response) => {
-		console.log('response', response);
+fetch('http://localhost:3001/contacts')
+	.then(async (response) => {
+		const json = await response.json();
+		console.log('response', json);
+		json.forEach((contact) => {
+			console.log(contact.name);
+		})
 	})
 	.catch((error) => {
 		console.log('erro', error);
